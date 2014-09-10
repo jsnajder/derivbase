7faa9f2341cce42a971ec9556beaa23c34da2868
Jan Snajder <jan.snajder@fer.hr>
2014-09-10 11:17:59 +0200

    Lemma.hs changes

--- src/DerivBase.hs
+++ src/DerivBase.hs
@@ -8,23 +8,10 @@
 module DerivBase 
   ( module DerivBase.DerivPairs
   , module DerivBase.DerivSets
-  , module DerivBase.LemmaPos ) where
+  , module DerivBase.Lemma ) where
 
 --import DerivBase.DerivGraph
 import qualified DerivBase.DerivPairs
 import qualified DerivBase.DerivSets
-import DerivBase.LemmaPos
-
-------------------------------------------------------------------------------
--- tmp:
-
-data POS   = Nm | Nf | Nn | N | A | V deriving (Show, Read, Eq, Ord, Enum)
-instance Pos POS where
-  posShow   = show
-  posRead   = read
-  posValues = [toEnum 0..]
-
---load = DS.fromFile "/home/jan/dismods/derivbase/deriv-families/data/DErivBase-v1.4.1-families.txt" :: IO (DS.DerivSets (LP POS))
-
--- derivFamily df (LP "Wasser" Nn)
+import DerivBase.Lemma
 
--- src/DerivBase/Lemma.hs
+++ src/DerivBase/Lemma.hs
@@ -1,14 +1,26 @@
 {-------------------------------------------------------------------------------
 
- DerivBase.LemmaPos
+ DerivBase.Lemma
  (c) 2014 Jan Snajder <jan.snajder@fer.hr>
 
 -------------------------------------------------------------------------------}
 
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module DerivBase.LemmaPos where
+module DerivBase.Lemma where
 
+class Lemma l where
+  readLemma :: String -> l
+  showLemma :: l -> String
+
+data LP p = LP String p deriving (Show, Ord, Eq)
+
+instance (Read p, Show p) => Lemma (LP p) where
+  readLemma s = let (l,_:p) = break (=='_') s in LP l (read p)
+  showLemma (LP l p) = l ++ "_" ++ show p
+
+{- OLD:
+ 
 class Pos p where
   posShow   :: p -> String
   posRead   :: String -> p
@@ -26,10 +38,8 @@ class LemmaPos lp where
   -- todo: make parsing more robust
   lpRead s = let (l,_:p) = break (=='_') s in lp l p
 
-data LP p = LP String p deriving (Show, Ord, Eq)
-
 instance Pos p => LemmaPos (LP p) where
   lp l p         = LP l (posRead p)
   lemma (LP l _) = l
   pos   (LP _ p) = posShow p
-
+-}
